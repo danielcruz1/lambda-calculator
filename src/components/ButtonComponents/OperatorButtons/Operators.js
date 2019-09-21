91eb916a7ff753804f1a8048fa9f99f4e6266893
staccats <47672588+staccats@users.noreply.github.com>
2019-09-21 01:59:35 -0600

    React local host rendering issues

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,4 +1,4 @@
-import React, {useState}  from "react";
+import React from "react";
 import OperatorButton from './OperatorButton';
 import { operators } from '../../../data';
 
@@ -8,16 +8,16 @@ import { operators } from '../../../data';
 
 const Operators = () => {
   // STEP 2 - add the imported data to state
-  const [opt] = useState(operators);
+ 
 
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
-       {opt.map((data, index) => (
-         <OperatorButton key={index} operatorData={data.char} />
-       ))}
+       {
+        operators.map(operator => <OperatorButton character= {operator.char} /> )
+      }
     </div>
   );
 };
--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -1,9 +1,15 @@
 import React from "react";
+import {specials} from "../../../data";
 
 const SpecialButton = () => {
   return (
-    <>
+    <div>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+      {
+       specials.map(character => <SpecialButton character={character} /> )
+       }
+    </div>
   );
 };
+
+export default SpecialButton;
